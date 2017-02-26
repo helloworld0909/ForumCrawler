f6ac32d29fdf36ba5266f43d15e33fb83964d4a2
Zheng Luo <helloworld09@163.com>
2017-02-26 20:27:49 +0800

    v0.32 Optimize the process of creating tables Changes: * TABLE_INFO format changed in settings.py * Dynamically create tables in pipelines.py * Add a rule in the spider
    Signed-off-by: Zheng Luo <helloworld09@163.com>

--- ForumCrawler/pipelines.py
+++ ForumCrawler/pipelines.py
@@ -5,10 +5,12 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 import MySQLdb
+
 from ForumCrawler import settings
 
 
 class MySQLPipeline(object):
+    # TODO insert many tuples each time, executemany
     def open_spider(self, spider):
         self.conn = MySQLdb.connect(
             host=settings.MYSQL_HOST,
@@ -20,8 +22,16 @@ class MySQLPipeline(object):
         )
         self.conn.autocommit(True)
         self.cur = self.conn.cursor()
-        for table_name, table_items in settings.TABLE_INFO.items():
-            self.cur.execute('create table if not exists {}({}) engine=MyISAM'.format(table_name, table_items))
+        for table_name, table_items in settings.TABLE_INFO.iteritems():
+            sql = 'create table if not exists {}({})'
+            table_items_sql = ','.join([k + ' ' + v for k, v in table_items['attrs'].iteritems()])
+            if 'pk' in table_items and table_items['pk']:  # Primary key(pk)
+                table_items_sql += ',' + 'primary key({})'.format(','.join(table_items['pk']))
+            if 'fk' in table_items and table_items['fk']:  # Foreign key(fk) references table(attr)
+                table_items_sql += ',' + 'foreign key({d[0]}) references {d[1]}({d[2]})'.format(d=table_items['fk'])
+            if 'engine' in table_items and table_items['engine']:   # Engine=SOMETYPE
+                sql += ' engine={}'.format(table_items['engine'])
+            self.cur.execute(sql.format(table_name, table_items_sql))
 
     def close_spider(self, spider):
         self.conn.commit()
